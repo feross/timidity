335dc12f38c787fe9583d9a7accb32e5266e6932
Feross Aboukhadijeh <feross@feross.org>
2018-06-20 18:43:30 -0700

    cleaner build script

--- tools/build-libtimidity.sh
+++ tools/build-libtimidity.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
 
-FLAGS="-Oz -s STRICT=1 -s ENVIRONMENT=web" # PRODUCTION
-# FLAGS="-g4 -DTIMIDITY_DEBUG" # DEBUG
+BUILD_FLAGS="-s STRICT=1 -s ALLOW_MEMORY_GROWTH=1"
+EXPORT_FLAGS="-s MODULARIZE=1 -s EXPORT_NAME=LibTimidity -s EXPORTED_FUNCTIONS=@exports.json -s EXTRA_EXPORTED_RUNTIME_METHODS=@exports-runtime.json"
 
-BUILD_OPTS="-s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME=\"'LibTimidity'\""
+OPTIMIZE_FLAGS="-Oz -s ENVIRONMENT=web" # PRODUCTION
+# OPTIMIZE_FLAGS="-g4 -DTIMIDITY_DEBUG" # DEBUG
 
-emcc -o libtimidity.js $FLAGS $BUILD_OPTS libtimidity/src/*.c -s EXPORTED_FUNCTIONS=@exports.json -s EXTRA_EXPORTED_RUNTIME_METHODS="['FS', 'Pointer_stringify']"
+emcc -o libtimidity.js $OPTIMIZE_FLAGS $BUILD_FLAGS $EXPORT_FLAGS libtimidity/src/*.c
