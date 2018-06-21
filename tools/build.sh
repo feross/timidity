580fadc4088786a19b765073a4875a11ad9b2e96
Feross Aboukhadijeh <feross@feross.org>
2018-06-20 19:34:15 -0700

    improve build script

--- tools/build.sh
+++ tools/build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-# Compile the libtimidity C codebase to JavaScript with emscripten
+# Build the package
 set -e
 
-BUILD_FLAGS="-s STRICT=1 -s ALLOW_MEMORY_GROWTH=1"
-EXPORT_FLAGS="-s MODULARIZE=1 -s EXPORT_NAME=LibTimidity -s EXPORTED_FUNCTIONS=@exports.json -s EXTRA_EXPORTED_RUNTIME_METHODS=@exports-runtime.json"
+# Compile the libtimidity C codebase to JavaScript with emscripten
+BUILD_FLAGS="-s STRICT=1 -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME=LibTimidity -s EXPORTED_FUNCTIONS=@exports.json -s EXTRA_EXPORTED_RUNTIME_METHODS=@exports-runtime.json"
 
+# Maximize optimization options for smallest file size
 OPTIMIZE_FLAGS="-Oz -s ENVIRONMENT=web" # PRODUCTION
 # OPTIMIZE_FLAGS="-g4 -DTIMIDITY_DEBUG" # DEBUG
 
-emcc -o libtimidity.js $OPTIMIZE_FLAGS $BUILD_FLAGS $EXPORT_FLAGS libtimidity/src/*.c
+emcc -o libtimidity.js $OPTIMIZE_FLAGS $BUILD_FLAGS libtimidity/src/*.c
