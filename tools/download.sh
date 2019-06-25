5c127e2aeb27bac65d56c4275f4763c1f3939f06
Feross Aboukhadijeh <feross@feross.org>
2019-06-25 14:21:30 -0700

    fix download tool

--- tools/download.sh
+++ tools/download.sh
@@ -3,13 +3,15 @@
 set -e
 
 rm -rf libtimidity
+rm -rf tmp
+mkdir tmp
 
-curl -L https://sourceforge.net/projects/libtimidity/files/latest/download -o /tmp/libtimidity.tar.gz
+curl -L https://sourceforge.net/projects/libtimidity/files/latest/download -o tmp/libtimidity.tar.gz
 
 # Extract e.g. libtimidity.tar.gz to libtimidity-0.2.5
-tar xf /tmp/libtimidity.tar.gz
+tar xf tmp/libtimidity.tar.gz
 
 # Rename e.g. libtimidity-0.2.5 to libtimidity
 mv libtimidity-* libtimidity
 
-rm libtimidity.tar.gz
+rm -rf tmp
