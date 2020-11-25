2b7491eca5dea20ed05adf87ea7ad674931d708f
Feross Aboukhadijeh <feross@feross.org>
2020-11-25 13:42:47 -1000

    fix promise usage. enhance source map

--- tools/build.sh
+++ tools/build.sh
@@ -3,13 +3,17 @@
 set -e
 
 # Compile the libtimidity C codebase to JavaScript with emscripten
-BUILD_FLAGS="-s STRICT=1 -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME=LibTimidity -s EXPORTED_FUNCTIONS=@tools/exports.json -s EXTRA_EXPORTED_RUNTIME_METHODS=@tools/exports-runtime.json"
+BUILD_FLAGS="-s STRICT=1 -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME=LibTimidity -s EXPORTED_FUNCTIONS=@tools/exports.json -s EXTRA_EXPORTED_RUNTIME_METHODS=@tools/exports-runtime.json -s FORCE_FILESYSTEM=1"
 
 # Maximize optimization options for smallest file size
-OPTIMIZE_FLAGS="-Oz -s ENVIRONMENT=web" # PRODUCTION
-# OPTIMIZE_FLAGS="-g4 -DTIMIDITY_DEBUG" # DEBUG
+OPTIMIZE_FLAGS_PROD="-Oz -s ENVIRONMENT=web"
+OPTIMIZE_FLAGS_DEBUG="-g4 -DTIMIDITY_DEBUG"
 
-emcc -o libtimidity.js $OPTIMIZE_FLAGS $BUILD_FLAGS libtimidity/src/*.c
+emcc -o libtimidity.js $OPTIMIZE_FLAGS_PROD $BUILD_FLAGS libtimidity/src/*.c
+emcc -o libtimidity.debug.js $OPTIMIZE_FLAGS_DEBUG $BUILD_FLAGS libtimidity/src/*.c
 
 # Include the freepats config in the published package so `brfs` can inline it
 cp node_modules/freepats/freepats.cfg freepats.cfg
+
+# Enhance the source maps for libtimidity.debug.js
+node tools/enhance-source-map.js
