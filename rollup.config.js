bb4e5a14c9840f37ef559510746bf1bab6332e65
Mike Russo <6748794-prolegomenatoy1@users.noreply.gitlab.com>
2021-02-18 09:34:28 -0500

    fix dist bundle

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import babel from '@rollup/plugin-babel';
 export default {
   input: 'index.js',
   output: {
-    file: 'dist/index.js',
+    file: 'bundle.js',
     format: 'umd',
     name: 'timidity'
   },
