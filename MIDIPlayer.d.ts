065705801bc806edcc9cef97335892afca8b4e62
Mike Russo <6748794-prolegomenatoy1@users.noreply.gitlab.com>
2021-05-31 11:25:33 -0400

    fixed breaking issue with declartion file

--- MIDIPlayer.d.ts
+++ MIDIPlayer.d.ts
@@ -1,11 +1,9 @@
-export as namespace "phaser-timidity-plugin";
-
-export = MIDIPlayer
-
 declare class MIDIPlayer {
-    static async createMIDIPlayer(cfgURL?: string, acontext?: AudioContext): Promise<MIDIPlayer>
+    static createMIDIPlayer(cfgURL?: string, acontext?: AudioContext): Promise<MIDIPlayer>
     play(): void
     pause(): void
     seek(sec: number): void
     load(midiURL: string): void
 }
+
+export = MIDIPlayer
