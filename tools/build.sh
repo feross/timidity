d4b573ee0513d9a0ed7ad2d6e8d4db00a0cafbc5
Noteworthy Software, Inc. <online@noteworthysoftware.com>
2023-10-25 17:27:58 -0400

    Upgrade to Emscripten 3.1.47

--- tools/build.sh
+++ tools/build.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
-# Build the package
+# Build the package, exit on error
 set -e
 
 # Compile the libtimidity C codebase to JavaScript with emscripten
-BUILD_FLAGS="-s STRICT=1 -s FORCE_FILESYSTEM=1 -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME=LibTimidity -s EXPORTED_FUNCTIONS=@tools/exports.json -s EXTRA_EXPORTED_RUNTIME_METHODS=@tools/exports-runtime.json"
+BUILD_FLAGS="-s STRICT=1 -s FORCE_FILESYSTEM=1 -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME=LibTimidity -s EXPORTED_FUNCTIONS=@tools/exports.json -s EXPORTED_RUNTIME_METHODS=@tools/exports-runtime.json"
 
 # -s ASSERTIONS=1
 
 # Maximize optimization options for smallest file size
 OPTIMIZE_FLAGS_PROD="-Oz -s ENVIRONMENT=web"
-OPTIMIZE_FLAGS_DEBUG="-g4 -DTIMIDITY_DEBUG"
+OPTIMIZE_FLAGS_DEBUG="-gsource-map -DTIMIDITY_DEBUG"
 
 emcc -o libtimidity.js $OPTIMIZE_FLAGS_PROD $BUILD_FLAGS libtimidity/src/*.c
 emcc -o libtimidity.debug.js $OPTIMIZE_FLAGS_DEBUG $BUILD_FLAGS libtimidity/src/*.c
 
 # Include the freepats config in the published package so `brfs` can inline it
 # cp node_modules/freepats/freepats.cfg freepats.cfg
-
-# Enhance the source maps for libtimidity.debug.js
-node tools/enhance-source-map.js
--- tools/enhance-source-map.js
+++
@@ -1,8 +0,0 @@
-// emcc -g4 doesn't append sourcesContents, but it is simple enough to do manually.
-// Closest issue related to this, but is closed: https://github.com/emscripten-core/emscripten/issues/6535
-
-const fs = require('fs')
-
-const map = JSON.parse(fs.readFileSync('libtimidity.debug.wasm.map', 'utf-8'))
-map.sourcesContent = map.sources.map(source => fs.readFileSync(source, 'utf-8'))
-fs.writeFileSync('libtimidity.debug.wasm.map', JSON.stringify(map, null, 2))
